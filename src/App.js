4f39a09a9c230eb7a53a6f464276515613de6a82
mar <gim.marcello@gmail.com>
2021-08-15 19:57:04 +0200

    Added: basic data fetching and processing structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Chart from 'chart.js/auto';
 
-function App() {
+const endpoint = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-regioni.json";
+
+function extractFunction(dataset) {
+  return dataset.map((element) => {
+    return element["totale_casi"];
+  });
+}
+
+// function derivative(dataset) {
+
+// }
+
+function Graph() {
+  const labels = useState([]);
+  const graphData = useState([]);
+
+  useEffect(() => {
+    let ctx = document.getElementById('mainChart').getContext('2d');
+    let chart = new Chart(ctx, {
+      type: 'line',
+
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'CoVID-19 spread in Italy',
+          background: null,
+          borderColor: 'rgb(255, 99, 132)',
+          data: graphData,
+        }]
+      },
+      options: {}
+    });
+
+    return () => {
+      chart.destroy()
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainGraph">
+      <canvas id="mainChart" />
     </div>
   );
 }
+
+function App() {
+  const [graphData, setGraphData] = useState({});
+  // const [changeGraphData, setChangeGraphData] = useState({});
+
+  useEffect(() => {
+    async function fetchGraphData() {
+      const data = await fetch(endpoint);
+      const jsonData = await data.json();
+      const extractedData = extractFunction(jsonData);
+
+      setGraphData(extractedData);
+      // setDerivative(derivative(extractedData));      
+    }
+    fetchGraphData();
+  }, []);
+
+  return (
+    // if state is falsy then add loading 
+    <div>
+      <Graph labels={[]} data={graphData} />
+      <button onclick={() => { }}>Derivative</button>
+    </div >
+  );
+}
+
 
 export default App;
